18aa397891e31150dbed8c7f3c76ea4df2ee6914
Liam Middlebrook <liammiddlebrook@gmail.com>
2016-10-22 00:07:31 -0400

    Openshift Support + Flask GitHub

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 from missing_license import app
+from missing_license import init_db
 
 if __name__ == "__main__":
+    init_db()
     app.run(host=app.config['IP'], port=app.config['PORT'])
 
 application = app
--- missing_license/__init__.py
+++ missing_license/__init__.py
@@ -1,9 +1,23 @@
-import sys
+import os
 
 from flask import Flask
-from flask.ext.github import GitHub
+from flask_github import GitHub
+from flask_github import GitHubError
 
 import requests
+
+from flask import Flask
+from flask import request
+from flask import g
+from flask import session
+from flask import redirect
+from flask import url_for
+from flask import render_template
+from flask.ext.github import GitHub
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 
 issue_body = """
@@ -26,6 +40,7 @@ Hope this helps, and happy hacking!
 """
 
 app = Flask(__name__)
+
 if os.path.exists(os.path.join(os.getcwd(), "config.py")):
     app.config.from_pyfile(os.path.join(os.getcwd(), "config.py"))
 else:
@@ -34,6 +49,107 @@ else:
 # Disable SSL certificate verification warning
 requests.packages.urllib3.disable_warnings()
 
-@app.route("/")
+github = GitHub(app)
+
+# setup sqlalchemy
+engine = create_engine('sqlite:////tmp/github-flask.db')
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(200))
+    github_access_token = Column(String(200))
+
+    def __init__(self, github_access_token):
+        self.github_access_token = github_access_token
+
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user_id' in session:
+        g.user = User.query.get(session['user_id'])
+
+
+@app.after_request
+def after_request(response):
+    db_session.remove()
+    return response
+
+@app.route('/<path:path>')
+def static_proxy(path):
+    # send_static_file will guess the correct MIME type
+    return app.send_static_file(path)
+
+@app.route('/')
 def index():
-    return "Hello World!"
+    status = request.args.get('status')
+    return render_template("index.html",
+                           logged_in=g.user,
+                           status=status)
+
+@github.access_token_getter
+def token_getter():
+    user = g.user
+    if user is not None:
+        return user.github_access_token
+
+
+@app.route('/github-callback')
+@github.authorized_handler
+def authorized(access_token):
+    next_url = request.args.get('next') or url_for('index')
+    if access_token is None:
+        return redirect(next_url)
+
+    user = User.query.filter_by(github_access_token=access_token).first()
+    if user is None:
+        user = User(access_token)
+        db_session.add(user)
+    user.github_access_token = access_token
+    db_session.commit()
+
+    session['user_id'] = user.id
+    return redirect(next_url)
+
+
+@app.route('/login')
+def login():
+    if session.get('user_id', None) is None:
+        return github.authorize(scope="public_repo")
+    else:
+        return 'Already logged in'
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('index'))
+
+@app.route('/file', methods=['POST'])
+def file_issue():
+    data = \
+    {
+        'title': "Missing License!?",
+        'body': issue_body
+    }
+
+    username = request.form.get('username')
+    repo = request.form.get('repository')
+
+    try:
+        github.get('repos/' + username + '/' + repo)
+    except GitHubError:
+        return redirect('/?status=bad')
+
+    github.post('repos/' + username + '/' + repo + '/issues', data)
+    return redirect('/?status=ok')
